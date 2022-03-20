ab12d1cfe5b5f73f8b382980bd98999a7c6e39bc
Nadia Mareta <82803405+KorbanFidas2A@users.noreply.github.com>
2022-03-21 03:28:19 +0700

    perlu benerin getFiles()

--- src/FolderCrawling/BFS.cs
+++ src/FolderCrawling/BFS.cs
@@ -9,7 +9,7 @@ class BFS
         //terima input dulu nanti disesuaiin sama GUI
         string input_dict = @"d:\";
         bool flag = true;
-        string file_name = "answer.txt";
+        string file_name = "tucilstima.py";
         DirectoryInfo dict = new DirectoryInfo(input_dict);
         DirectoryInfo [] directories_list = dict.GetDirectories();
         //var filePaths = dict.EnumerateFiles(input_dict, new EnumerationOptions{
@@ -46,8 +46,9 @@ class BFS
     public static DirectoryInfo [] listDirectory(DirectoryInfo [] dict){
         int sum = 0;
         foreach (DirectoryInfo name in dict){
-            string directory = Path.GetDirectoryName(name.FullName);
-            DirectoryInfo new_dict = new DirectoryInfo(directory);
+            int string_length = (name.FullName).Length;
+            string nama_file = (name.FullName).Substring(2, string_length-2);
+            DirectoryInfo new_dict = new DirectoryInfo(nama_file);
             DirectoryInfo [] directories_list = new_dict.GetDirectories();
             sum += directories_list.Length;
 
@@ -56,8 +57,7 @@ class BFS
         DirectoryInfo [] final_list = new DirectoryInfo[sum];
         int current_idx = 0;
         foreach (DirectoryInfo name_list in dict){
-            string directory2 = Path.GetDirectoryName(name_list.FullName);
-            DirectoryInfo new_dict2 = new DirectoryInfo(directory2);
+            DirectoryInfo new_dict2 = new DirectoryInfo(name_list.FullName);
             DirectoryInfo [] directories_list2 = new_dict2.GetDirectories();
             directories_list2.CopyTo(final_list, current_idx);
             current_idx += directories_list2.Length;
@@ -72,8 +72,10 @@ class BFS
     public static bool searchDirectory(DirectoryInfo [] dict, string filename){
         bool result = false;
         foreach (DirectoryInfo name in dict){
-            string directory3 = Path.GetDirectoryName(name.FullName);
-            DirectoryInfo new_dict = new DirectoryInfo(directory3);
+            int string_length = (name.FullName).Length;
+            string nama = name.FullName;
+            string nama_file = nama.Substring(2, string_length-2);
+            DirectoryInfo new_dict = new DirectoryInfo(nama_file);
             if(searchDepth(new_dict, filename)){
                 result = true;
                 return result;
@@ -85,19 +87,14 @@ class BFS
     
     //untuk mencari nama file di directory
     public static bool searchDepth(DirectoryInfo dict2, string name){
-        string directory3 = Path.GetDirectoryName(dict2.FullName);
-        if(directory3  != null){
-            var filePaths = dict2.EnumerateFiles(directory3, new EnumerationOptions{
-                IgnoreInaccessible = true,
-                RecurseSubdirectories = true
-            });
-            foreach (var files in filePaths){
-                Console.WriteLine(files.Name);
-                if(files.Name == name){
-                    return true;
-                }
+        int string_length = (dict2.FullName).Length;
+        string nama = dict2.FullName;
+        string nama_file = nama.Substring(2, string_length-2);
+        var filePaths = dict2.EnumerateFiles();
+        foreach (var files in filePaths){
+            if(files.Name == name){
+                return true;
             }
-            return false;
         }
         return false;
     }
