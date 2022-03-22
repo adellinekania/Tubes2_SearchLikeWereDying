b5d6630632eefa7b1663c8b39c49b8d2c1cc269c
Nadia Mareta <82803405+KorbanFidas2A@users.noreply.github.com>
2022-03-22 12:10:45 +0700

    BFS non all-occurence + tree config

--- src/FolderCrawling/BFS.cs
+++ src/FolderCrawling/BFS.cs
@@ -11,215 +11,83 @@ namespace FolderCrawling
         private GUIOutput output = new GUIOutput();
 
 
-        public void searchBFS(List<DirectoryTree> treeArray, 
-            int index, 
-            Microsoft.Msagl.Drawing.Graph graph, 
-            string searchDir, 
+        public void useBFS(DirectoryTree tree,
+            Microsoft.Msagl.Drawing.Graph graph,
+            string searchDir,
             ComboBox comboBoxFile,
             RichTextBox textFolderRoute)
         {
-            if(index < treeArray.Count)
+            List<DirectoryTree> tree_list = new List<DirectoryTree>();
+            //ini buat disamain sama algo BFS nya ya
+            for(int i = 0; i < tree.Count; i++)
             {
-                DirectoryTree tree = treeArray[index];
-                string treeDataPath = tree.Data;
-                string treeData = Path.GetFileName(tree.Data);
-                tree.changeData(treeData + " " + "(" + countNode + ")");
-                countNode++;
-                //printAllPath(treeData, index, textFilePath);
-
-                if(!isFound)
-                {
-                    if (treeData.Equals(searchDir))
-                    {
-                        output.displayTreeDirs(tree, graph, "blue");
-                        comboBoxFile.Items.Add(treeDataPath);
-                        isFound = true;
-                    }
-                    else
-                    {
-                        output.displayTreeDirs(tree, graph, "red");
-                    }
-                    output.printFolderRoute(treeData, textFolderRoute);
-                   
-                }
-
-                else
-                {
-                    output.displayTreeDirs(tree, graph, "black");
-                }
-
-                if (!isFound)
-                {
-                    int countChild = tree.Count;
-                    for (int i = 0; i < countChild; i++)
-                    {
-                        treeArray.Add(tree[i]);
-                    }
-                }
-                index++;
-                searchBFS(treeArray, index, graph, searchDir, comboBoxFile, textFolderRoute);
+                tree_list.Add(tree[i]);
             }
-        }
 
-        public void searchBFSAllOccur(List<DirectoryTree> treeArray, 
-            int index, 
-            Microsoft.Msagl.Drawing.Graph graph, 
-            string searchDir, 
-            ComboBox comboBoxFile,
-            RichTextBox textFolderRoute)
-        {
-            if(index < treeArray.Count)
+            while(tree_list.Count != 0 && !isFound)
             {
-                DirectoryTree tree = treeArray[index];
-                string treeDataPath = tree.Data;
-                string treeData = Path.GetFileName(tree.Data);
-                tree.changeData(treeData + " " + "(" + countNode + ")");
-                countNode++;
-                //printAllPath(treeData, index, textFilePath);
-
-                if(isFound){
-                    output.displayTreeDirs(tree, graph, "black");
-                    isFound = false;
-                }
-
-                if (treeData.Equals(searchDir))
+                if(searchTreeList(tree_list, searchDir, graph, comboBoxFile, textFolderRoute))
                 {
-                    output.displayTreeDirs(tree, graph, "blue");
-                    comboBoxFile.Items.Add(treeDataPath);
                     isFound = true;
                 }
                 else
                 {
-                    output.displayTreeDirs(tree, graph, "red");
+                    tree_list = arrayDir(tree_list);
                 }
-                output.printFolderRoute(treeData, textFolderRoute);
-
-                int countChild = tree.Count;
-                for (int i = 0; i < countChild; i++)
-                {
-                    treeArray.Add(tree[i]);
-                }
-                index++;
-                searchBFS(treeArray, index, graph, searchDir, comboBoxFile, textFolderRoute);
             }
+
         }
 
+        public List<DirectoryTree> arrayDir (List<DirectoryTree> treeArray)
+        {
+            int total_length = 0;
+            int idx = 0;
+            for(int i = 0; i < treeArray.Count; i++)
+            {
+                total_length += treeArray[i].Count;
+            }
+            List<DirectoryTree> array = new List<DirectoryTree>();
+            for(int i = 0; i < treeArray.Count; i++)
+            {
+                DirectoryTree childTree = treeArray[i];
+                for(int j = 0; j < treeArray[i].Count; j++)
+                {
+                    array.Add(childTree[j]);
+                    idx++;
+                }
+            }
 
+            return array;
+        }
 
+        public bool searchTreeList(List<DirectoryTree> treeArray, string nameDir, Microsoft.Msagl.Drawing.Graph graph,
+            ComboBox comboBoxFile, RichTextBox textFolderRoute)
+        {
+            bool found = false;
+            int idx = 0;
+            for(int i = 0; i < treeArray.Count; i++)
+            {
+                string namefile = Path.GetFileName(treeArray[i].Data);
+                treeArray[i].changeData(namefile + " " + "(" + countNode + ")");
+                countNode++;
+                if (namefile == nameDir)
+                {
+                    output.displayTreeDirs(treeArray[i], graph, "blue");
+                    comboBoxFile.Items.Add(treeArray[i].Data);
+                    isFound = true;
+                    found = true;
+                    idx = i;
+                    break;
+                }
+                else
+                {
+                    output.displayTreeDirs(treeArray[i], graph, "red");
+                }
+                output.printFolderRoute(treeArray[i].Data, textFolderRoute);
 
-        // public static void Main()
-        // {
-        //     //terima input dulu nanti disesuaiin sama GUI
-        //     string input_dict = @"d:\";
-        //     bool flag = true;
-        //     string file_name = "tucilstima.py";
-        //     DirectoryInfo dict = new DirectoryInfo(input_dict);
-        //     DirectoryInfo [] directories_list = dict.GetDirectories();
-        //     //var filePaths = dict.EnumerateFiles(input_dict, new EnumerationOptions{
-        //     //        IgnoreInaccessible = true,
-        //     //        RecurseSubdirectories = true
-        //     //});
-        //     //foreach (var files in filePaths){
-        //     //    Console.WriteLine(files.Name);
-        //     //}
-
-
-        //     if(!searchDepth2(dict, file_name)){
-        //         //Console.WriteLine("Ini apa");
-        //         while (directories_list.Length != 0 && flag){
-        //             if(searchDirectory(directories_list, file_name)){
-        //                 flag = false;
-        //             }else{
-        //                 directories_list = listDirectory(directories_list);
-        //             }
-        //         }
-        //     }else{
-        //         flag = false;
-        //     }
-        //     if(flag){
-        //         Console.WriteLine("File tidak ditemukan.");
-        //     }
-
-        // } 
-
-        
-        // //Mencari seluruh directory yang mungkin
-        // public static DirectoryInfo [] listDirectory(DirectoryInfo [] dict){
-        //     int sum = 0;
-        //     foreach (DirectoryInfo name in dict){
-        //         int string_length = (name.FullName).Length;
-        //         string nama_file = (name.FullName).Substring(2, string_length-2);
-        //         DirectoryInfo new_dict = new DirectoryInfo(nama_file);
-        //         if(nama_file != "\\System Volume Information"){
-        //             DirectoryInfo [] directories_list = new_dict.GetDirectories();
-        //             sum += directories_list.Length;
-        //         }
-        //     }
-
-        //     DirectoryInfo [] final_list = new DirectoryInfo[sum];
-        //     int current_idx = 0;
-        //     foreach (DirectoryInfo name_list in dict){
-        //         int string_length = (name_list.FullName).Length;
-        //         string nama_file = (name_list.FullName).Substring(2, string_length-2);
-        //         DirectoryInfo new_dict2 = new DirectoryInfo(nama_file);
-        //         if(nama_file != "\\System Volume Information"){
-        //             DirectoryInfo [] directories_list2 = new_dict2.GetDirectories();
-        //             directories_list2.CopyTo(final_list, current_idx);
-        //             current_idx += directories_list2.Length;
-        //         }
-        //     }
-
-        //     return final_list;
-        // }
-
-
-
-        // //mencari kebenaran apakah file tersebut ada atau tidak pada masing-masing directory
-        // public static bool searchDirectory(DirectoryInfo [] dict, string filename){
-        //     bool result = false;
-        //     foreach (DirectoryInfo name in dict){
-        //         int string_length = (name.FullName).Length;
-        //         string nama = name.FullName;
-        //         string nama_file = nama.Substring(2, string_length-2);
-        //         DirectoryInfo new_dict = new DirectoryInfo(nama_file);
-        //         //Console.WriteLine(nama_file);
-        //         if(nama_file != "\\System Volume Information"){
-        //             if(searchDepth(new_dict, filename)){
-        //                 result = true;
-        //                 return result;
-        //             }
-        //         }
-        //     }
-        //     return false;
-        // }
-
-        
-        // //untuk mencari nama file di directory
-        // public static bool searchDepth(DirectoryInfo dict2, string name){
-        //     int string_length = (dict2.FullName).Length;
-        //     string nama = dict2.FullName;
-        //     string nama_file = nama.Substring(2, string_length-2);
-        //     var filePaths = dict2.EnumerateFiles();
-        //     foreach (var files in filePaths){
-        //         if(files.Name == name){
-        //             Console.WriteLine(dict2.FullName);
-        //             return true;
-        //         }
-        //     }
-        //     return false;
-        // }
-
-        // //Khusus untuk kasus pertama karena SearchDepth gak bisa ngehandle root folder
-        // public static bool searchDepth2(DirectoryInfo dict2, string name){
-        //     FileInfo [] file = dict2.GetFiles();
-        //     foreach (FileInfo f1 in file){
-        //         //Console.WriteLine(f1.Name);
-        //         if(f1.Name == name){
-        //             return true;
-        //         }
-        //     }
-        //     return false;
-        // }
-    } 
-    
+            }
+          
+            return found;
+        }
+    }
 }
